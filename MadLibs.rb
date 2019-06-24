dfb6d85c83999405152ea1d837e932e4eaec7629
Vortex009 <45364830+Vortex009@users.noreply.github.com>
2019-06-24 12:04:39 -0400

    Add files via upload

---
+++ MadLibs.rb
@@ -0,0 +1,75 @@
+puts "Enter a Man's Name: "
+mans_name = gets.chomp
+puts "Enter an Occupation: "
+occupation = gets.chomp
+puts "Enter a Noun: "
+noun = gets.chomp
+puts "Enter a Noun: "
+noun2 = gets.chomp
+puts "Enter a Noun: "
+noun3 = gets.chomp
+puts "Enter a Shape: "
+shape = gets.chomp
+puts "Enter a Man's Name: "
+mans_name2 = gets.chomp
+puts "Enter a Verb: "
+verb = gets.chomp
+puts "Enter a Woman's Name: "
+womans_name = gets.chomp
+puts "Enter a Body Part: "
+body_part = gets.chomp
+puts "Enter a Verb: "
+verb2 = gets.chomp
+puts "Enter a Noun: "
+noun4 = gets.chomp
+puts "Enter a Noun: "
+noun5 = gets.chomp
+puts "Enter a Restaurant Name: "
+restaurant_name = gets.chomp
+puts "Enter a Historic Monument: "
+historic_monument = gets.chomp
+puts "Enter a Verb: "
+verb3 = gets.chomp
+puts "Enter a Noun: "
+noun6 = gets.chomp
+puts "Enter a Noun: "
+noun7 = gets.chomp
+puts "Enter a Noun: "
+noun8 = gets.chomp
+puts "Enter a Verb: "
+verb4 = gets.chomp
+puts "Enter a Noun: "
+noun9 = gets.chomp
+puts "Enter an Adjective: "
+adjective = gets.chomp
+puts "Enter an Adjective: "
+adjective2 = gets.chomp
+puts "Enter an Emotion: "
+emotion = gets.chomp
+puts "Enter a Verb: "
+verb5 = gets.chomp
+puts "Enter a Noun: "
+noun10 = gets.chomp
+puts "Enter a Noun: "
+noun11 = gets.chomp
+puts "Enter a Verb: "
+verb6 = gets.chomp
+
+puts "#{mans_name} is a normal #{occupation}. Then, one day, a"
+puts "#{noun} explodes, causing a #{noun2} to blow up, and a nearby"
+puts "#{noun3} erupts into a #{shape} of flames. #{mans_name} realizes that"
+puts "he's being chased by the government, who's trying to #{verb} him. While"
+puts "on the run, he teams up with an incredibly attractive woman named"
+puts "#{womans_name}, who has an incredible #{body_part}. She may be"
+puts "from the streets, but she can #{verb2} like nobody's business. The duo"
+puts "decide to turn the tables on their pursuers by blowing up a #{noun4}, which"
+puts "triggers a chain reaction, causing the local #{noun5}, #{restaurant_name}, and"
+puts "the #{historic_monument} to explode. Then, the bad guys' helicopter gets"
+puts "#{verb3} by a piece of #{noun6} from when the #{noun7} exploded,"
+puts "and the helicopter explodes and falls onto a #{noun8}, causing it to #{verb4},"
+puts "which shoots a fireball straight into the heart of #{noun9} and destroys the"
+puts "bad guy leader. Everything is #{adjective} and the two decide that such a"
+puts "#{adjective2} ordeal has caused them to fall in #{emotion} with each other."
+puts "They decide to celebrate by #{verb5}ing on the #{noun10}, and they even"
+puts "managed to use a #{noun11} from the beginning of the movie, to #{verb6}"
+puts "the whole story together."
